39e14f92ff35b9bc8488d23355fbfd55d030260d
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-11-11 14:45:36 +0300

    code refactoring

--- p33113/s264452/lab1/main.c
+++ p33113/s264452/lab1/main.c
@@ -36,7 +36,7 @@ threadFunc(void *restrictArg)
 
     fread(arg->writeAddr, 1, arg->size, arg->dataSource);
     if (arg->logEnabled) {
-        printf("Filling address: %p - pid: %u \t| tid: %lu\n", arg->writeAddr, getpid(), pthread_self());
+        fprintf(stderr, "Filling address: %p - pid: %u \t| tid: %lu\n", arg->writeAddr, getpid(), pthread_self());
     }
 
     pthread_exit((void *)0);
@@ -44,8 +44,8 @@ threadFunc(void *restrictArg)
 
 void
 systemFree() {
-    system("free");
-    puts("");
+    system("free 1>&2");
+    fprintf(stderr, "\n");
 }
 
 // fills memory with mmap function
@@ -56,21 +56,19 @@ fmem(void *addr, size_t size, int logEnabled)
     int err;
     
     if (logEnabled) {
-        puts("\nBefore allocation:");
+        fprintf(stderr, "\nBefore allocation:");
         systemFree();
     }
 
-    void * mmapAddr;
-    mmapAddr = mmap(addr, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    void * mmapAddr = mmap(addr, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     
     if (logEnabled) {
-        puts("\nAfter allocation: ");
+        fprintf(stderr, "\nAfter allocation: ");
         systemFree();
     }
 
     pthread_t threads [THREADS_AMOUNT] ;
     struct threadFuncArg * arg;
-    
     
     for (size_t i = 0; i < THREADS_AMOUNT; i++) {
         
@@ -83,7 +81,7 @@ fmem(void *addr, size_t size, int logEnabled)
 
         if (err != 0)
         {
-            puts("Error with creating thread");
+            fprintf(stderr, "Error with creating thread");
             exit(err);
         }
    
@@ -99,10 +97,10 @@ fmem(void *addr, size_t size, int logEnabled)
     }
 
     if (logEnabled) {
-        puts("\nAfter filling: ");
+        fprintf(stderr, "\nAfter filling: ");
         systemFree();
     
-        printf("Compare mmap returned addr: %p and map addr: %p \n", mmapAddr, addr);
+        fprintf(stderr, "Compare mmap returned addr: %p and map addr: %p \n", mmapAddr, addr);
     }
 
     return mmapAddr;
@@ -133,19 +131,19 @@ void
 dumpMem(int fd, void * addr, int size, int * futex) {
     wait_on_futex_value(futex, 0);
     *futex = 0;
-    printf("Dump memory to %d\t from %p \t[size=%.2f MB]\n", fd, addr, size / (1024.0 * 1024.0));
+    fprintf(stderr, "Dump memory to %d\t from %p \t[size=%.2f MB]\n", fd, addr, size / (1024.0 * 1024.0));
 
-    int iterations = size / DATA_BLOCK_SIZE;
+    size_t iterations = size / DATA_BLOCK_SIZE;
     int tenPercent = iterations / 10;
     int errno;
     int res = ftruncate(fd, 0);
     if (res == -1) {
-        printf("truncate : %d\n", errno);
+        fprintf(stderr, "truncate : %d\n", errno);
     }
-    printf("Truncate file result: %d\n", res);
+    fprintf(stderr, "Truncate file result: %d\n", res);
     for (size_t i = 0; i < iterations; i++) {
         if (i % tenPercent == 0) {
-            printf("%ld0%% - %.2f MB\n", i / tenPercent, (size - i * DATA_BLOCK_SIZE) / (1024.0 * 1024.0));
+            fprintf(stderr, "%ld0%% - %.2f MB\n", i / tenPercent, (size - i * DATA_BLOCK_SIZE) / (1024.0 * 1024.0));
         }
         void * pointer = (uint8_t*) addr + i;
         write(fd, &pointer, DATA_BLOCK_SIZE);
@@ -157,7 +155,7 @@ dumpMem(int fd, void * addr, int size, int * futex) {
 void
 counter(unsigned int times) {
     for (int i = times; i > 0; i--) {
-        printf("...%d\n", i);
+        fprintf(stderr, "...%d\n", i);
         sleep(1);
     }
 }
@@ -220,7 +218,7 @@ aggregateFile(struct memoryDumpMap * args) {
         }
     }
 
-    printf("Calculated sum on file [%d]:\t %lu\n", args->fd, sum);
+    fprintf(stderr, "Calculated sum on file [%d]:\t %lu\n", args->fd, sum);
 
     *futex = 1;
 
@@ -241,16 +239,16 @@ readFileFunc(void * arg) {
 int main()
 {
     int fillMemSize = FILL_MEM_SIZE * 1024 * 1024;
-    puts("\nAllocating memory with mmap function");
+    fprintf(stderr, "\nAllocating memory with mmap function");
     void * mmapAddr = fmem((void *)ALLOC_ADDR, fillMemSize, 1);
 
     munmap(mmapAddr, fillMemSize);
 
-    puts("\nAfter deallocation");
+    fprintf(stderr, "\nAfter deallocation");
     systemFree();
 
     
-    void * memoryAddr = fmem((void *) ALLOC_ADDR, FILL_MEM_SIZE, 0);
+    const void * memoryAddr = fmem((void *) ALLOC_ADDR, FILL_MEM_SIZE, 0);
     
     size_t dumpMemSize = DUMP_FILE_SIZE * 1024 * 1024;
     int filesAmount = fillMemSize / dumpMemSize + (fillMemSize % dumpMemSize > 0 ? 1 : 0);
@@ -267,12 +265,14 @@ int main()
         files[i] = open(filename, O_RDWR | O_CREAT, (mode_t) 0600);
 
         if (files[i] == -1) {
-            puts("Error: Can't create or open file");
+            fprintf(stderr, "Error: Can't create or open file");
         }
+
         args->dumpMap[i] = malloc(sizeof(struct memoryDumpMap));
         args->dumpMap[i]->fd = files[i];
         args->dumpMap[i]->addr = (uint8_t*) memoryAddr + i * (dumpMemSize / 8);
         args->dumpMap[i]->size = dumpMemSize;
+
         int shm_id = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0666);
         args->dumpMap[i]->futex = shmat(shm_id, NULL, 0);
     }
@@ -285,9 +285,9 @@ int main()
 
     pthread_attr_setschedparam(memDTA, memDP);
     pthread_create(&memDumpTP, NULL, dumpMemThreadFunc, args);
-    puts("Writing thread created...");
+    fprintf(stderr, "Writing thread created...\n");
     
-    puts("Creating reading threads");
+    fprintf(stderr, "Creating reading threads\n");
     pthread_t counters[COUNTERS_THREAD_AMOUNT];
      
     pthread_attr_t * memAttr = malloc(sizeof(pthread_attr_t));
@@ -299,14 +299,14 @@ int main()
         counters[i] = pthread_create(&counters[i], NULL, readFileFunc, args); 
     }
 
-    printf("Created %d aggregator threads\n", COUNTERS_THREAD_AMOUNT);
+    fprintf(stderr, "Created %d aggregator threads\n", COUNTERS_THREAD_AMOUNT);
 
     int times = 3;
-    printf("Unlocking all blocks after %d seconds\n", times);
+    fprintf(stderr, "Unlocking all blocks after %d seconds\n", times);
     counter(times);
 
 
-    puts("Unlocking all blocks...");
+    fprintf(stderr, "Unlocking all blocks...\n");
     for (int i = 0; i < filesAmount; i++) {
         int * futex = args->dumpMap[i]->futex;
         wake_futex_blocking(futex, 1);
@@ -314,7 +314,7 @@ int main()
 
     int err = pthread_join(memDumpTP, NULL);
     if (err != 0) {
-        printf("Error pthred_join. Status: %d\n", err);
+        fprintf(stderr, "Error pthred_join. Status: %d\n", err);
     }
 
     for (int i = 0; i < COUNTERS_THREAD_AMOUNT; i++) {
