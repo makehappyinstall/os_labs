2534a8e22362212d8b8c42193f82f9e5dff5488a
marypodsekina <56410438+marypodsekina@users.noreply.github.com>
2020-12-24 00:28:45 +0300

    Update FileIO.c

--- p33113/264474/lab1/FileIO.c
+++ p33113/264474/lab1/FileIO.c
@@ -1,28 +1,4 @@
-
-
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/file.h>
-#include <rpc.h>
-
-#define blockSize    95//G
-#define blockNumbersCount   blockSize / sizeof(int) //G
-#define blockNumbersSize  blockNumbersCount *sizeof(int)
-
-union IOBlock {
-    char raw[blockSize];
-    int numbers[blockNumbersCount];
-};
-
-
-struct FileWriterThreadParams {
-    int file_id;
-    int *startAddress;
-    size_t blocksCount;
-    int *infinityLoop;
-    pthread_mutex_t *fileMutex;
-};
+#include "FileIO.h"
 
 void *FileWriterThread(void *rawParams) {
 
@@ -33,39 +9,30 @@ void *FileWriterThread(void *rawParams) {
     int random_offset;
     char *file_name = malloc(256);
     snprintf(file_name, 256, "labfile_%d", params->file_id);
-    printf("%s\r\n", file_name);
+    printf("\n%s\r", file_name);
 
     memset(block.raw, 0, blockSize);
+
     while (*params->infinityLoop) {
 
         address = params->startAddress;
         pthread_mutex_lock(params->fileMutex);
 
         file = fopen(file_name, "wb");
-        printf("%d\n",params->blocksCount*blockSize);
         for (size_t i = 0; i < params->blocksCount; ++i) {
-            
+
             random_offset = (rand()%params->blocksCount)*blockNumbersSize;
             memcpy(block.numbers, address + random_offset, blockNumbersSize);
             fwrite(block.raw, sizeof(char), blockSize, file);
 
         }
+
         fclose(file);
         pthread_mutex_unlock(params->fileMutex);
 
     }
     return NULL;
 }
-
-
-struct FileReaderThreadParams {
-    int filesCount;
-    int *infinityLoop;
-    long long int sum;
-
-    struct FileWriterThreadParams *fileWriterThreadParams;
-};
-
 
 void *FileReaderThread(void *rawParams) {
 
@@ -85,9 +52,7 @@ void *FileReaderThread(void *rawParams) {
 
             pthread_mutex_lock(params->fileWriterThreadParams[i].fileMutex);
             file = fopen(file_name, "rb");
-            memset(block.raw, 0, blockSize);
-            rewind(file);
-
+//            memset(block.raw, 0, blockSize);
 
             for (size_t j = 0; j < params->fileWriterThreadParams[i].blocksCount; ++j) {
 
@@ -98,7 +63,6 @@ void *FileReaderThread(void *rawParams) {
                     params->sum += block.numbers[k];
 
                 }
-
 
             }
             fclose(file);
