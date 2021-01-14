08e702763af7cfb447f6fba56e06d64600c89023
Sergey Fedorov <sergeyfedorov2@gmail.com>
2021-01-14 22:22:27 +0300

    Moved lab1 to corresponding directory

---
+++ p33113/s263974/lab1/main.c
@@ -0,0 +1,31 @@
+//
+// Created by sdfedorov on 24/11/2020.
+//
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "memory_ops.h"
+#include "file_ops.h"
+#include "constants.h"
+#include "util.h"
+
+int main(){
+#ifndef DEBUG
+    srand(time(NULL));
+#endif
+
+    wait_for_input("ALLOCATE MEM");
+    void* mem_region = allocate_memory((void*) B, A * megabyte);
+    wait_for_input("FILL MEM");
+    fill_the_memory(mem_region, A * megabyte, "/dev/urandom", D);
+    wait_for_input("WRITE MEM TO FILES");
+    write_rnd_mem_to_files(mem_region, A * megabyte, E * megabyte, G);
+    wait_for_input("READ AND AGGREGATE FROM FILES");
+    long result = aggregate_value_from_files(A * megabyte, E * megabyte, I, LONG_MAX, &min);
+    printf("RESULTED VALUE: %ld\n", result);
+    wait_for_input("DEALLOCATE MEM");
+    deallocate_memory((void*) B, A * megabyte);
+    wait_for_input("END");
+}
---
+++ p33113/s263974/lab1/memory_ops.h
@@ -0,0 +1,16 @@
+//
+// Created by sdfedorov on 24/11/2020.
+//
+
+#include <stddef.h>
+
+#ifndef OPERATING_SYSTEMS_ITMO_2020_MEMORY_OPS_H
+#define OPERATING_SYSTEMS_ITMO_2020_MEMORY_OPS_H
+
+#define DEBUG
+
+void* allocate_memory(void* addr, size_t size);
+int deallocate_memory(void* addr, size_t size);
+void fill_the_memory(void* addr, size_t size, const char* read_from, int thread_count);
+
+#endif //OPERATING_SYSTEMS_ITMO_2020_MEMORY_OPS_H
